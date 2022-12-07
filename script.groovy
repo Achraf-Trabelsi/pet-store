7f2096268a0457abec4e83fc208cde8dc80d6323
achref Trabelsi <achref.trabelsi.insat@gmail.com>
2022-12-07 16:36:33 +0100

    feat: updated sonarscan function

--- script.groovy
+++ script.groovy
@@ -4,7 +4,7 @@ def pushToNexus() {
     sh 'mvn clean package -D maven.test.skip=true deploy:deploy-file -D groupId=org.springframework.samples -D artifactId=spring-petclinic -D version=2.7.3 -D packaging=jar -D repositoryId=deploymentRepo -D url=http://localhost:8081/repository/maven-releases/ -D file=target/spring-petclinic-2.7.3.jar'
 }
 
-def sonarScan(String serverIp, String serverUser) {
+def sonarScan() {
     echo "Running sonarQube scan..."
     sh 'mvn test'
     sh 'mvn sonar:sonar -D sonar.login=48d7556f97d0484c006e752b08112ebef5b318c5'
