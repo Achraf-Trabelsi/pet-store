c9a5cea053e8d422ec7115204d633e64f6101828
meryem18 <meryem.mahmoud@insat.u-carthage.tn>
2022-12-08 20:26:46 +0100

    changes2

--- script.groovy
+++ script.groovy
@@ -1,10 +1,9 @@
 
 def pushToNexus() {
     echo "pushing the jar file to Nexus maven-snapshots repo..."
-    bat 'mvn clean package -D maven.test.skip=true deploy:deploy-file -D groupId=org.springframework.samples -D artifactId=spring-petclinic -D version=2.7.3 -D packaging=jar -D repositoryId=deploymentRepo -D url=http://localhost:8081/repository/maven-releases/ -D file=target/spring-petclinic-2.7.3.jar'
+    bat 'mvn clean install -D maven.test.skip=true'
+    nexusArtifactUploader artifacts: [[artifactId: 'spring-petclinic', classifier: '', file: 'target/spring-petclinic-2.7.3.jar', type: 'jar']], credentialsId: 'nexus-credentials', groupId: 'org.springframework.samples', nexusUrl: 'localhost:8081', nexusVersion: 'nexus3', protocol: 'http', repository: '/maven-releases', version: '2.7.3'
 }
-
-
 def sonarScan() {
     echo "Run sonarQube scan..."
     bat "mvn clean verify sonar:sonar -D sonar.projectKey=projet -D maven.test.skip=true  -D sonar.login=sqp_5f53273b9bc7a25846d21721bb48b488011f10d8"
