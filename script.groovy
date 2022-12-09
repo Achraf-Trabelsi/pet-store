1176cee739a80f9cca5e5e3d11232a454b306958
insafml <insaf.mellakh@gmail.com>
2022-12-09 11:34:20 +0100

    test-sonar

--- script.groovy
+++ script.groovy
@@ -1,4 +1,3 @@
-
 def pushToNexus() {
     echo "pushing the jar file to Nexus maven-snapshots repo..."
     sh 'mvn clean package -D maven.test.skip=true deploy:deploy-file -D groupId=org.springframework.samples -D artifactId=spring-petclinic -D version=2.7.3 -D packaging=jar -D repositoryId=deploymentRepo -D url=http://localhost:8081/repository/maven-releases/ -D file=target/spring-petclinic-2.7.3.jar'
@@ -7,7 +6,7 @@ def pushToNexus() {
 def sonarScan() {
     echo "Running sonarQube scan and testing ..."
     sh 'mvn test'
-    sh 'mvn sonar:sonar -D sonar.login=48d7556f97d0484c006e752b08112ebef5b318c5'
+    sh 'mvn sonar:sonar -D sonar.login=squ_eb2ee702c84ca8f40765caac79cadb46aca17ecc'
 }
 
 return this
