19199435fc2681494d1c6821a3fb8dfee882ba0c
chedlyba <chedly.benazizi@insat.u-carthage.tn>
2022-12-09 10:59:23 +0100

    aaaaaa1

--- Jenkinsfile
+++ Jenkinsfile
@@ -19,7 +19,7 @@ pipeline {
             steps {
                 script {
                     gv.sonarScan()
-                    sh 'echo sonarqube'
+                    bat 'echo sonarqube'
                 }
             }
         }
--- script.groovy
+++ script.groovy
@@ -1,13 +1,13 @@
 
 def pushToNexus() {
     echo "pushing the jar file to Nexus maven-snapshots repo..."
-    sh 'mvn clean package -D maven.test.skip=true deploy:deploy-file -D groupId=org.springframework.samples -D artifactId=spring-petclinic -D version=2.7.3 -D packaging=jar -D repositoryId=deploymentRepo -D url=http://localhost:8081/repository/maven-releases/ -D file=target/spring-petclinic-2.7.3.jar'
+    bat 'mvn clean package -D maven.test.skip=true deploy:deploy-file -D groupId=org.springframework.samples -D artifactId=spring-petclinic -D version=2.7.3 -D packaging=jar -D repositoryId=deploymentRepo -D url=http://localhost:8081/repository/maven-releases/ -D file=target/spring-petclinic-2.7.3.jar'
 }
 
 def sonarScan() {
     echo "Running sonarQube scan and testing ..."
-    sh 'mvn test'
-    sh 'mvn sonar:sonar -D sonar.login=48d7556f97d0484c006e752b08112ebef5b318c5'
+    bat 'mvn test'
+    bat 'mvn sonar:sonar -D sonar.login=48d7556f97d0484c006e752b08112ebef5b318c5'
 }
 
 return this
